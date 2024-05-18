22f15e454624bb772c9cc95cf09346bdfdd9e082
gleniomontovani <gleniomontovani@gmail.com>
2024-05-18 17:09:02 -0300

    Ajustando numero da porta de conexao do serviço

--- src/main/java/br/com/postech/techchallenge/microservico/pedido/configuration/ControllerMappingConfig.java
+++ src/main/java/br/com/postech/techchallenge/microservico/pedido/configuration/ControllerMappingConfig.java
@@ -3,5 +3,5 @@ package br.com.postech.techchallenge.microservico.pedido.configuration;
 public class ControllerMappingConfig {
 
 	public final static String PATH_API_PAGAMENTO = "/v1/pagamentos";
-	public final static String ENDPOINT_MICRO_SERVICE_PEDIDO_LOCAL = "http://localhost:8080/v1/pedidos";
+	public final static String ENDPOINT_MICRO_SERVICE_PEDIDO_LOCAL = "http://localhost:8081/v1/pedidos";
 }
